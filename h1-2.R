3a97d9ecedabec026278e4e73bd934a39f407e94
thomasmaltese <thomasmaltese64@gmail.com>
2018-02-19 17:49:46 -0500

    completed homework 1

--- h1-2.R
+++ h1-2.R
@@ -3,13 +3,4 @@
 # problem 2
 set.seed(200)
 x=rnorm(25,10,4)
-
 print(t.test(x, mu=10))
-
-# the p-value is = 0.9143
-# we need a p-value < 0.05 to reject the null hypothesis
-# therefore we fail to reject Ho
-# the large p-value means that, if the null hypothesis were
-# true, there is a 'p-value' probability of observing the 
-# test statistic. since we observed a test statistic of 0.10881
-# it is very likely that the null hypothesis is true
--- h1-3.R
+++ h1-3.R
@@ -1,24 +1,20 @@
 # Tom Maltese
 # HW 1
 # problem 3
-
 df = read.table("data", sep=",")
 colnames(df) = c("rent","size")
-
 covar = cov(df)
 correl = cor(df)
-
 print(covar[1,2])
 print(correl[1,2])
-
 y=df$rent
 x=df$size
 model = lm(y~x)
 print(summary(model))
-
 coeffs = coef(model)
 yint = unname(coeffs[1])
 slope = unname(coeffs[2]) 
-
+print(yint)
+print(slope)
 input = 1050
 print((yint + slope * input))
